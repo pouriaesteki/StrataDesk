b977424c3979dbd56e47bbee0b530570d45165b8
pouriaesteki <76649664+pouriaesteki@users.noreply.github.com>
2025-06-24 08:09:29 -0700

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await prisma.user.findUnique({
-      where: { id: decoded.userId }
+      where: { id: decoded.id }
     });
 
     if (!user) {
